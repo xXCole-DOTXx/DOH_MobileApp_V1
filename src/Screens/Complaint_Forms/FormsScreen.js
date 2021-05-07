94c3fc11ecdcbe3a6db3d8a90b78c53a2ac044fd
Cole Perry <E096752@wv.gov>
2021-05-07 13:35:01 -0400

    Card css looking good

--- src/Screens/Complaint_Forms/FormsScreen.js
+++ src/Screens/Complaint_Forms/FormsScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, FlatList, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements'
+import { styles } from './styles.js';
 
 const FormsScreen = ({navigation, route}) => {
 
@@ -20,13 +21,27 @@ const FormsScreen = ({navigation, route}) => {
         <Text>WV DOT Employees.</Text>
         <FlatList 
         keyExtractor={(item) => item.ID }
+        style = {styles.List}
         data={formsArray}
         renderItem={({item}) => (
           <Card>
             <Card.Title>{item.ID}</Card.Title>
             <Card.Divider/>
-            <View>
-              <Text>{item.Name}</Text>
+            <View style={styles.Container}>
+              <Text style={styles.Comments}>{item.Comments}</Text>
+           
+              <Text style={styles.Comments}>{item.RoadName}</Text>
+            </View>
+
+            <View style={styles.ListContainer}>
+              <Text style={styles.LabelText}>Name</Text>
+              <Text style={styles.LabelText}>Phone</Text>
+              <Text style={styles.LabelText}>Email</Text>
+            </View>
+            <View style={styles.ListContainer}>
+              <Text style={styles.CardText}>{item.Name}</Text>
+              <Text style={styles.CardText}>{item.Phone}</Text>
+              <Text style={styles.CardText}>{item.Email}</Text>
             </View>
           </Card>
         )}
---
+++ src/Screens/Complaint_Forms/styles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+
+//Helpful video on Flexbox: https://www.youtube.com/watch?v=R2eqAgR_KlU&t=802s
+
+const styles = StyleSheet.create({
+    Container: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    ListContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    CardText: {
+        padding: 10
+    },
+    LabelText: {
+        padding: 10,
+        fontWeight: "bold"
+    },
+    Comments: {
+
+    },
+    Image: {
+
+    },
+    List: {
+        flex: 1,
+        padding: 10
+    }
+})
+
+export {styles}
