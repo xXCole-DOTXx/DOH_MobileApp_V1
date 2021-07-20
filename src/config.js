caaf2b40a7c0920c74fb3fe8be73434778adc30e
xXCole-DOTXx <cole.k.perry@wv.gov>
2021-07-20 11:28:35 -0400

    Start ignoring config files

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    keyPrefix: 'Uploaded Photos/',
-    bucket: 'rn-mobile-app-bucket',
-    region: 'us-east-2',
-    accessKey: 'AKIA3OOUK4FCMI33OUFG',
-    secretKey: '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',
-    successActionStatus: 201
-  }
